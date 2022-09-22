f8a37b79ac44ecbadcdfe090959f16f39bb8780f
vishhu <vishnu403.reddy@gmail.com>
2022-09-22 22:20:10 +0530

    updating ./learning-shell

--- 04.sh
+++ 04.sh
@@ -7,3 +7,14 @@ echo "git push origin branch_name
                                   echo "04_sh_example"
                                   echo "git checkout -b branch_name"
                                   echo "git push origin branch_name"
+
+
+                                  echo "04_sh_example"
+                                  echo "git checkout -b branch_name"
+                                  echo "git push origin branch_name
+                                  "echo "04_sh_example"
+                                                                    echo "git checkout -b branch_name"
+                                                                    echo "git push origin branch_name"
+                                                                    echo "04_sh_example"
+                                                                    echo "git checkout -b branch_name"
+                                                                    echo "git push origin branch_name"
