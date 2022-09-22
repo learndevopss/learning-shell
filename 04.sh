d98ce15f09c899fc3a429bdddc83dfc0f3f22017
vishhu <vishnu403.reddy@gmail.com>
2022-09-22 22:22:34 +0530

    updating ./learning-shell

--- 04.sh
+++ 04.sh
@@ -17,4 +17,24 @@ echo "git push origin branch_name
                                                                     echo "git push origin branch_name"
                                                                     echo "04_sh_example"
                                                                     echo "git checkout -b branch_name"
-                                                                    echo "git push origin branch_name"
+
+                            echo "04_sh_example"
+                            echo "git checkout -b branch_name"
+                            echo "git push origin branch_name
+                            "echo "04_sh_example"
+                                                              echo "git checkout -b branch_name"
+                                                              echo "git push origin branch_name"
+                                                              echo "04_sh_example"
+                                                              echo "git checkout -b branch_name"
+                                                              echo "git push origin branch_name"
+
+
+                                                              echo "04_sh_example"
+                                                              echo "git checkout -b branch_name"
+                                                              echo "git push origin branch_name
+                                                              "echo "04_sh_example"
+                                                                                                echo "git checkout -b branch_name"
+                                                                                                echo "git push origin branch_name"
+                                                                                                echo "04_sh_example"
+                                                                                                echo "git checkout -b branch_name"
+                                                                                                echo "git push origin branch_name"                                        echo "git push origin branch_name"
