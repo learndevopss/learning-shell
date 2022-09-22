e88cb23cdfaa15695f963855fe92b9ea574fdd72
vishhu <vishnu403.reddy@gmail.com>
2022-09-22 22:19:37 +0530

    auto commit

--- 04.sh
+++ 04.sh
@@ -4,3 +4,6 @@ echo "git push origin branch_name
 "echo "04_sh_example"
                                   echo "git checkout -b branch_name"
                                   echo "git push origin branch_name"
+                                  echo "04_sh_example"
+                                  echo "git checkout -b branch_name"
+                                  echo "git push origin branch_name"
